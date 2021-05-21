2894800ad242f3c0d92d981260f78286b9554d86
Artorivs <abcarvalho@uchicago.edu>
2021-05-21 18:32:40 -0500

    minor changes to formatting

--- data-scripts/trace_nb.jl
+++ data-scripts/trace_nb.jl
@@ -1,3 +1,4 @@
+# %%
 using DataFrames
 using Printf
 using CSV
@@ -10,7 +11,7 @@ modules_path = string(main_path, "/modules")
 script_path = string(main_path, "/data-scripts")
 include(string(joinpath(modules_path, "data_module"), ".jl"))
 
-# * Inputs
+# %% Inputs
 # Mergent
 rmnp=true
 min_maturity = 20111231
@@ -21,15 +22,15 @@ eval_merge=true
 
 # TRACE
 yr = 2019
-qtr = 1
+qtr = 3
 
 # Dates
 miss_date = 11111111
 
-# * Load Objects and Data
-tro = DataMod.trace_obj_constructor()
+# %% Load Objects and Data
+tro = DataMod.data_obj_constructor()
 
-# ** Mergent Bond Issue + Rating Data
+# %% Mergent Bond Issue + Rating Data
 # this will filter the mergent data to keep
 # only the bonds with maturity > min_maturity
 # and rating data from a rating agency in rating_types.
@@ -42,11 +43,36 @@ mdf = @time DataMod.get_mergent_fisd_df(tro;
                                          eval_merge=eval_merge,
                                          miss_date=miss_date)
 
+# %% Load Trace Filtered DataFrame
+filter_fpath = string(tro.tr.trace_path, "/", tro.tr.filter_dir)
+filter_fname = string(tro.tr.filter_file_prefix, "_", yr, "_Q", qtr, ".csv")
+tdf = @time DataFrame!(CSV.File(string(filter_fpath, "/", filter_fname), types=tro.tr.colsd))
 
-# ** Load Trace Filtered DataFrame
-filter_fpath = string(tro.trace_path, "/", tro.filter_dir)
-filter_fname = string(tro.filter_file_prefix, "_", yr, "_Q", qtr, ".csv")
-tdf = @time DataFrame!(CSV.File(string(filter_fpath, "/", filter_fname), types=tro.colsd))
+# %% Merge TRACE and MERGENT DataFrames, run IG classifier
+fdf, mdiag = @time DataMod.merge_trace_mergent_dfs(tro, tdf, mdf)
 
-# * Merge TRACE and MERGENT DataFrames, run IG classifier
-fdf = @time DataMod.merge_trace_mergent_dfs(tro, tdf, mdf)
+
+# %%
+tr_cusips = unique(tdf[:, :cusip_id])
+mf_cusips = unique(mdf[:, :COMPLETE_CUSIP])
+println(string("Unique CUSIPS in trace dataframe: ", size(tr_cusips, 1)))
+println(string("Unique CUSIPS in FISD dataframe: ", size(mf_cusips, 1)))
+
+# %% Unmatched CUSIPs
+mtc = intersect(tr_cusips, mf_cusips)
+umtc = setdiff(tr_cusips, mtc)
+
+# %%
+size(mtc, 1)/size(tr_cusips,1)
+size(umtc, 1)/size(tr_cusips,1)
+
+# %% List characteristics of the unmatched bonds
+cols = [:cusip_id, :bond_sym_id, :company_symbol, :scrty_type_cd, :bloomberg_identifier, :sub_prdct]
+idx_cusips = findall(in(umtc), tdf[:, :cusip_id])
+tmp = unique(tdf[idx_cusips, cols])
+
+# %%
+first(tmp, 20)
+
+# %%
+unique(tmp[:, :sub_prdct])
